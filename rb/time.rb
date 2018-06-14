f13a93d1fe9096462edfe4aa02e0798b80eb44ba
Aneks <ks03151103@yahoo.co.jp>
2018-06-14 22:34:54 +0900

    add a directory and anything

--- rb/time.rb
+++ rb/time.rb
@@ -243,4 +243,31 @@ class Time
           \s*\z/ix)
         self.rfc2822(date).utc
       elsif /\A\s*
-        
+          (?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday),\x20
+          (\d\d)-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-(\d\d)\x20
+          (\d\d):(\d\d):(\d\d)\x20
+          GMT
+          \s*\z/ix =~ date
+        year = $3.to_i
+        if year < 50
+          year += 2000
+        else
+          year += 1900
+        end
+        self.utc(year, $2, $1.to_i, $4.to_i, $5.to_i, $6.to_i)
+      elsif /\A\s*
+          (?:Mon|Tue|Wed|Thu|Fri|Sat|Sun)\x20
+          (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\x20
+          (\d\d|\x20\d)\x20
+          (\d\d):(\d\d):(\d\d)\x20
+          (\d{4})
+          \s*\z/ix =~ date
+        self.utc($6.to_i, MonthValue[$1.upcase], $2.to_i,
+                    $3.to_i, $4.to_i, $5.to_i)
+      else
+        raise ArgumentError.new("not RFC 2616 compliment date: #{date.inspect}")
+      end
+    end
+
+    def xmlschema(date)
+      
